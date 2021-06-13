744b11cc951bb915287b24b86e97880029c5f181
何秀奇 <hshowq@126.com>
2021-06-13 15:45:00 +0800

    新增websocket通信

---
+++ Commun.NetWork/WSocketClient.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocket4Net;
+using ZCS_Common;
+
+namespace Commun.NetWork
+{
+    public class WSocketClient : IDisposable
+    {
+
+        //向外传递数据事件
+        public event Action<string> MessageReceived;
+
+        WebSocket4Net.WebSocket _webSocket;
+        /// <summary>
+        /// 检查重连线程
+        /// </summary>
+        Thread _thread;
+        bool _isRunning = true;
+        /// <summary>
+        /// WebSocket连接地址
+        /// </summary>
+        public string ServerPath
+        {
+            get; set;
+        }
+
+        public WSocketClient(string url)
+        {
+            ServerPath = url;
+            this._webSocket = new WebSocket4Net.WebSocket(url);
+            this._webSocket.Opened += WebSocket_Opened;
+            //this._webSocket.Error += WebSocket_Error;
+            this._webSocket.Closed += WebSocket_Closed;
+            this._webSocket.MessageReceived += WebSocket_MessageReceived;
+        }
+
+        #region "web socket "
+        /// <summary>
+        /// 连接方法
+        /// <returns></returns>
+        public bool Start()
+        {
+            bool result = true;
+            try
+            {
+                this._webSocket.Open();
+
+                this._isRunning = true;
+                this._thread = new Thread(new ThreadStart(CheckConnection));
+                this._thread.Start();
+            }
+            catch (Exception ex)
+            {
+                LogHelper.WriteException(ex);
+                result = false;
+            }
+            return result;
+        }
+        /// <summary>
+        /// 消息收到事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            LogHelper.WriteInfoLog(" Received:" + e.Message, "WSocketInf");
+            MessageReceived?.Invoke(e.Message);
+        }
+        /// <summary>
+        /// Socket关闭事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_Closed(object sender, EventArgs e)
+        {
+            LogHelper.WriteInfoLog("websocket_Closed", "WSocketInf");
+        }
+        /// <summary>
+        /// Socket报错事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_Error(object sender, ErrorEventArgs e)
+        {
+            LogHelper.WriteException(e.GetException());
+        }
+        /// <summary>
+        /// Socket打开事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void WebSocket_Opened(object sender, EventArgs e)
+        {
+            LogHelper.WriteInfoLog("websocket_Opened", "WSocketInf");
+        }
+        /// <summary>
+        /// 检查重连线程
+        /// </summary>
+        private void CheckConnection()
+        {
+            do
+            {
+                try
+                {
+                    if (this._webSocket.State != WebSocket4Net.WebSocketState.Open && this._webSocket.State != WebSocket4Net.WebSocketState.Connecting)
+                    {
+                        LogHelper.WriteInfoLog(" Reconnect websocket WebSocketState:" + this._webSocket.State, "WSocketInf");
+                        this._webSocket.Close();
+                        this._webSocket.Open();
+                        Console.WriteLine("正在重连");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.WriteException(ex);
+                }
+                System.Threading.Thread.Sleep(5000);
+            } while (this._isRunning);
+        }
+        #endregion
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="Message"></param>
+        public void SendMessage(string Message)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if (_webSocket != null && _webSocket.State == WebSocket4Net.WebSocketState.Open)
+                {
+                    this._webSocket.Send(Message);
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            this._isRunning = false;
+            try
+            {
+                _thread.Abort();
+            }
+            catch
+            {
+
+            }
+
+            if (this._webSocket is not null)
+            {
+                this._webSocket.Close();
+                this._webSocket.Dispose();
+            }
+            this._webSocket = null;
+        }
+    }
+}
---
+++ CommunTools/FrmBaseCom/Frm_ComWebSocket.cs
@@ -0,0 +1,128 @@
+﻿using Com_CSSkin;
+using Com_CSSkin.SkinControl;
+using Commun.NetWork;
+using Commun.NetWork.MQTT;
+using CommunTools.Common;
+using CommunTools.Entity;
+using CommunTools.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ZCS_Common;
+using ZCS_FormUI.Forms;
+using sp = CommunTools.Enums.SerialPortEnum;
+
+namespace CommunTools
+{
+    public partial class Frm_ComWebSocket : CSSkinMain
+    {
+        public Frm_ComWebSocket()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        WSocketClient wsClient;
+
+        private void Frm_SerialServer_Load(object sender, EventArgs e)
+        {
+            JsonFileConfig jsonFile = new JsonFileConfig();
+            jsonFile.ConfigSection = "TCPServer";
+
+            var jc = jsonFile.LoadJsonConfig(jsonFile.ConfigFile, "TCPServer");
+
+            txtTCPIP.InputText = jc[0];
+        }
+
+        private void btnStart_BtnClick(object sender, EventArgs e)
+        {
+            OpenWSServer();
+        }
+
+        private void OpenWSServer()
+        {
+            if (!"断开链接".Equals(btnStart.BtnText))
+            {
+                //新建客户端类 
+                //服务端IP地址 ws://192.168.1.13 如果服务端开启了ssl或者tsl 这里前缀应该改成 wss:/
+                //服务端监听端口 1234
+                //自定义的地址参数 可以根据地址参数来区分客户端 /lcj控制台
+                //开始链接
+
+                try
+                {
+                    wsClient = new WSocketClient((ckbTLS.Checked ? "wss://" : "ws://") + txtTCPIP.InputText);
+                    wsClient.Start();
+                    ShowMsg("链接成功！");
+                    MessageReceived();
+                }
+                catch (Exception ex)
+                {
+                    ShowMsg($"发生异常链接失败{ex.ToString()}");
+                    throw;
+                }
+
+                btnStart.BtnText = "断开链接";
+            }
+            else
+            {
+                // 记得释放资源否则会造成堆栈
+                wsClient.Dispose();
+                ShowMsg("已成功释放资源!");
+                MessageReceived();
+
+                btnStart.BtnText = "链接服务";
+            }
+        }
+
+        /// <summary>
+        /// 服务端返回的消息
+        /// </summary>
+        private void MessageReceived()
+        {
+            //注册消息接收事件，接收服务端发送的数据
+            wsClient.MessageReceived += (data) => {
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("[{0}]# \n", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                sb.AppendFormat(data + "\n");
+
+                txtShowMsg.Text += sb.ToString();
+            };
+        }
+     
+        void ShowMsg(string str)
+        {
+            txtShowMsg.AppendText(str + "\r\n");
+        }
+
+        private void btnSend_BtnClick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(richTextBox_Send.Text.Trim()))
+            {
+                string inputMsg = richTextBox_Send.Text.ToString();
+              
+                wsClient.SendMessage(inputMsg);
+                MessageReceived();
+            }
+        }
+
+        private void Frm_ComWebSocket_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            wsClient.Dispose();
+        }
+    }
+}
